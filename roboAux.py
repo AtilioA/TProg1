7bc6c16e31ebed618e25e8b4124aec826b6c2807
AtilioA <atiliodadalto@hotmail.com>
2019-06-09 15:42:56 -0300

    Módulo auxiliar

---
+++ roboAux.py
@@ -0,0 +1,47 @@
+# Módulo com funções auxiliares
+# Autores: Atílio Antônio Dadalto
+# Disciplina: Programação I, 2019/1, ministrada por Jordana Sarmenghi Salamon em 2019/
+
+
+def tupla1(tupla4):
+    ''' Função: tupla1()
+    Descrição: retorna o primeiro elemento de uma tupla de 4 elementos
+    Escopo: função global paramétrica
+    Dados de entrada: tupla de 4 elementos
+    Dados de saída: primeiro elemento da tupla
+    '''
+
+    return tupla4[0]
+
+
+def tupla2(tupla4):
+    ''' Função: tupla2()
+    Descrição: retorna o segundo elemento de uma tupla de 4 elementos
+    Escopo: função global paramétrica
+    Dados de entrada: tupla de 4 elementos
+    Dados de saída: segundo elemento da tupla
+    '''
+
+    return tupla4[1]
+
+
+def tupla3(tupla4):
+    ''' Função: tupla3()
+    Descrição: retorna o terceiro elemento de uma tupla de 4 elementos
+    Escopo: função global paramétrica
+    Dados de entrada: tupla de 4 elementos
+    Dados de saída: terceiro elemento da tupla
+    '''
+
+    return tupla4[2]
+
+
+def tupla4(tupla4):
+    ''' Função: tupla4()
+    Descrição: retorna o quarto (último) elemento de uma tupla de 4 elementos
+    Escopo: função global paramétrica
+    Dados de entrada: tupla de 4 elementos
+    Dados de saída: quarto (último) elemento da tupla
+    '''
+
+    return tupla4[-1]
